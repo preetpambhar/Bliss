f2519e8548bf8d146baed15f75a9d56a7bc003c4
Preet Pambhar <preetpambhar73530@gmail.com>
2024-10-02 18:23:50 -0400

    feat: search component

---
+++ Bliss/View/LocationSearch/Views/LocationSearchActivation.swift
@@ -0,0 +1,33 @@
+//
+//  LocationSearchActivation.swift
+//  Bliss
+//
+//  Created by Preet Pambhar on 2024-10-02.
+//
+
+import SwiftUI
+
+struct LocationSearchActivation: View {
+    var body: some View {
+        HStack{
+            Rectangle()
+                .fill(Color.black)
+                .frame(width: 8, height: 8)
+                .padding(.horizontal)
+            Text("Search delivery address")
+                .foregroundColor(Color(.darkGray))
+            
+            Spacer()
+        }
+        .frame(width: UIScreen.main.bounds.width - 20, height: 50)
+        .background(
+            Rectangle()
+                .fill(Color.white)
+                .shadow(color: .black, radius: 6)
+        )
+    }
+}
+
+#Preview {
+    LocationSearchActivation()
+}
--- Bliss/View/TabViews/Home.swift
+++ Bliss/View/TabViews/Home.swift
@@ -8,39 +8,43 @@
 import SwiftUI
 
 struct Home: View {
+    @State private var searchText: String = ""
     var body: some View {
+        
      //   Base view
         NavigationView{
             ScrollView(.vertical, showsIndicators: false){
-                VStack(alignment: .leading, spacing: 20) {
-                    Text("Good Morning")
-                        .font(.title)
-
-                    HStack {
-                        CustomCrousel(content: [
-                                                Image("flowers")
-                                                    .resizable()
-                                                    .aspectRatio(contentMode: .fill)
-                                                    .cornerRadius(15) ,
-                                                Image("bliss")
-                                                    .resizable()
-                                                    .aspectRatio(contentMode: .fill)
-                                                    .cornerRadius(15),
-                                                Image("flowers")
-                                                    .resizable()
-                                                    .aspectRatio(contentMode: .fill)
-                                                    .cornerRadius(15)
-                                            ])
-                        .frame(height: 200)
-                    } 
-                    
-                    Image("flowers")
-                        .resizable()
-                        .aspectRatio(contentMode: .fill)
-                        .frame(height: 380)
-                    .cornerRadius(15)
-                }
-                .padding()
+                    VStack(alignment: .leading, spacing: 20) {
+                        Text("Good Morning")
+                            .font(.title)
+                        
+                        LocationSearchActivation()
+                        
+                        HStack {
+                            CustomCrousel(content: [
+                                                    Image("flowers")
+                                                        .resizable()
+                                                        .aspectRatio(contentMode: .fill)
+                                                        .cornerRadius(15) ,
+                                                    Image("bliss")
+                                                        .resizable()
+                                                        .aspectRatio(contentMode: .fill)
+                                                        .cornerRadius(15),
+                                                    Image("flowers")
+                                                        .resizable()
+                                                        .aspectRatio(contentMode: .fill)
+                                                        .cornerRadius(15)
+                                                ])
+                            .frame(height: 200)
+                        }
+                        
+                        Image("flowers")
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(height: 380)
+                        .cornerRadius(15)
+                    }
+                    .padding()
             }
             .navigationTitle("Home")
         }
