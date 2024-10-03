429b7b378cf8211eb2c8d5c00f07d1b54bd79d8c
Preet Pambhar <preetpambhar73530@gmail.com>
2024-10-03 03:17:31 -0400

    feat: location manager

---
+++ Bliss/Managers/LocationManager.swift
@@ -0,0 +1,27 @@
+//
+//  LocationManager.swift
+//  Bliss
+//
+//  Created by Preet Pambhar on 2024-10-03.
+//
+
+import CoreLocation
+
+class LocationManager: NSObject, ObservableObject {
+    private let locationManager = CLLocationManager()
+    
+    override init() {
+        super.init()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+}
+
+extension LocationManager: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard !locations.isEmpty else  { return }
+        locationManager.startUpdatingLocation()
+    }
+}
