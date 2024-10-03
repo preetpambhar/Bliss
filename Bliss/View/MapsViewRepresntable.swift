917b3e7b4664c552d85064b3210d4b4d7ef2241e
Preet Pambhar <preetpambhar73530@gmail.com>
2024-10-03 03:18:41 -0400

    feat: get location

---
+++ Bliss/View/MapsViewRepresntable.swift
@@ -0,0 +1,52 @@
+//
+//  mapsRepresntable.swift
+//  Bliss
+//
+//  Created by Preet Pambhar on 2024-10-03.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapViewRepresentable: UIViewRepresentable {
+    
+    let mapView = MKMapView()
+    let locationManager = LocationManager()
+    
+    func makeUIView(context: Context) -> some UIView {
+        mapView.delegate = context.coordinator
+        mapView.isRotateEnabled = false
+        mapView.showsUserLocation = true
+        mapView.userTrackingMode = .follow
+        
+        return mapView
+    }
+    
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+        
+    }
+    
+    func makeCoordinator() -> MapCoordinator {
+        return MapCoordinator(parent: self)
+    }
+}
+
+extension MapViewRepresentable {
+    class MapCoordinator: NSObject, MKMapViewDelegate {
+        let parent: MapViewRepresentable
+        
+        init(parent: MapViewRepresentable){
+            self.parent = parent
+            super.init()
+        }
+        
+        func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+            let region = MKCoordinateRegion(
+                center: CLLocationCoordinate2D(latitude: userLocation.coordinate.latitude, longitude: userLocation.coordinate.longitude),
+                span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+            )
+            
+            parent.mapView.setRegion(region, animated: true)
+        }
+    }
+}
