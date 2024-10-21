a646726ae433d8f8b991d9382301bbe6f7bd0a8b
Preet Pambhar <preetpambhar73530@gmail.com>
2024-10-21 00:07:54 -0400

    feat: extention

--- Bliss/Helpers/Double+Extention.swift
+++ Bliss/Helpers/Double+Extention.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+extension Double{
+    func toString() -> String{
+        return String(format: "%.1f",self)
+    }
+    func currencyFormat() -> String{
+        let formatter = NumberFormatter()
+        formatter.maximumFractionDigits = 2
+        formatter.minimumFractionDigits = 2
+        formatter.numberStyle = .currency
+        return formatter.string(from: NSNumber(value: self)) ?? ""
+    }
+}
