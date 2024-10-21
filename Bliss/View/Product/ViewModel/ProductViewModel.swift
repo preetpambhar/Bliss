8d2d8d499c9cab3b3b212ca8a01000593272241c
Preet Pambhar <preetpambhar73530@gmail.com>
2024-10-21 00:09:11 -0400

    feat: demo product model

--- Bliss/View/Product/ViewModel/ProductViewModel.swift
+++ Bliss/View/Product/ViewModel/ProductViewModel.swift
@@ -5,14 +5,19 @@
 //  Created by Preet Pambhar on 2024-10-11.
 //
 
-import SwiftUI
+import Foundation
 
-struct ProductViewModel: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+@Observable class ProductViewModel {
+    
+    var product: [Product] = []
+    private let manager = APIManager()
+    
+    func fetchProducts () async{
+        do{
+            product = try await manager.request(url: "https://fakestoreapi.com/products")
+            print(product)
+        }catch{
+            print("Fetch Product Error:", error)
+        }
     }
-}
-
-#Preview {
-    ProductViewModel()
 }
